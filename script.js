ec8bdc3b0db1e338b71833344cb9dc1f27952abc
yuriika123 <yuta.gen1219.guitar@gmail.com>
2025-07-25 04:58:47 +0900

    delete

--- script.js
+++
@@ -1,6 +0,0 @@
-const openButton = document.getElementById("open-button") 
-
-openButton.addEventListener("click", () => {
-    console.log("open")
-    window.location.href = 'about/about.html';
-})
